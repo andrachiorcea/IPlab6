0c827ca5cb40f09e39df782bf404c0e17321ad15
oana-hriscu <oana.hriscu97@gmail.com>
2018-03-28 11:41:55 +0300

    tests

--- src/componentTests/TestRunner.java
+++ src/componentTests/TestRunner.java
@@ -13,12 +13,17 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(GPanelTest.class);
-		
+      
+      
+	   Result result= JUnitCore.runClasses(SearchElementTest.class);
+	  
       for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-		
+          System.out.println(failure.toString());
+       }
+	  
+      
       System.out.println(result.wasSuccessful());
+      
+      
    }
 } 
